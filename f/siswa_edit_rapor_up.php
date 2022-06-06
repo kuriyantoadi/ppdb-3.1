a37b3553a753cd80fbf560b1d2d707a0db4fd23c
kuriyantoadi <melatikuning212@gmail.com>
2022-06-06 23:00:22 +0700

    update 6 juni 2022, update 14

--- f/siswa_edit_rapor_up.php
+++ f/siswa_edit_rapor_up.php
@@ -2,8 +2,6 @@
 // koneksi database
 include '../koneksi.php';
 
-
-session_start();
 if ($_SESSION['status']=="akl") {
   $kompetensi_keahlian = "Akuntansi dan Keuangan Lembaga";
   $kode_jur = "akl";
@@ -39,32 +37,32 @@ $sem1_ipa = isset($_POST['sem1_ipa']) ? abs((int) $_POST['sem1_ipa']) :0;
 $sem1_b_ing = isset($_POST['sem1_b_ing']) ? abs((int) $_POST['sem1_b_ing']) :0;
 
 //semester 2
-$sem2_agama = mysql_real_escape_string($_POST['sem2_agama']);
-$sem2_b_indo = mysql_real_escape_string($_POST['sem2_b_indo']);
-$sem2_mtk = mysql_real_escape_string($_POST['sem2_mtk']);
-$sem2_ipa = mysql_real_escape_string($_POST['sem2_ipa']);
-$sem2_b_ing = mysql_real_escape_string($_POST['sem2_b_ing']);
-
-//semester 3
-$sem3_agama = mysql_real_escape_string($_POST['sem3_agama']);
-$sem3_b_indo = mysql_real_escape_string($_POST['sem3_b_indo']);
-$sem3_mtk = mysql_real_escape_string($_POST['sem3_mtk']);
-$sem3_ipa = mysql_real_escape_string($_POST['sem3_ipa']);
-$sem3_b_ing = mysql_real_escape_string($_POST['sem3_b_ing']);
-
-//semester 4
-$sem4_agama = mysql_real_escape_string($_POST['sem4_agama']);
-$sem4_b_indo = mysql_real_escape_string($_POST['sem4_b_indo']);
-$sem4_mtk = mysql_real_escape_string($_POST['sem4_mtk']);
-$sem4_ipa = mysql_real_escape_string($_POST['sem4_ipa']);
-$sem4_b_ing = mysql_real_escape_string($_POST['sem4_b_ing']);
-
-//semester 5
-$sem5_agama = mysql_real_escape_string($_POST['sem5_agama']);
-$sem5_b_indo = mysql_real_escape_string($_POST['sem5_b_indo']);
-$sem5_mtk = mysql_real_escape_string($_POST['sem5_mtk']);
-$sem5_ipa = mysql_real_escape_string($_POST['sem5_ipa']);
-$sem5_b_ing = mysql_real_escape_string($_POST['sem5_b_ing']);
+// $sem2_agama = mysql_real_escape_string($_POST['sem2_agama']);
+// $sem2_b_indo = mysql_real_escape_string($_POST['sem2_b_indo']);
+// $sem2_mtk = mysql_real_escape_string($_POST['sem2_mtk']);
+// $sem2_ipa = mysql_real_escape_string($_POST['sem2_ipa']);
+// $sem2_b_ing = mysql_real_escape_string($_POST['sem2_b_ing']);
+//
+// //semester 3
+// $sem3_agama = mysql_real_escape_string($_POST['sem3_agama']);
+// $sem3_b_indo = mysql_real_escape_string($_POST['sem3_b_indo']);
+// $sem3_mtk = mysql_real_escape_string($_POST['sem3_mtk']);
+// $sem3_ipa = mysql_real_escape_string($_POST['sem3_ipa']);
+// $sem3_b_ing = mysql_real_escape_string($_POST['sem3_b_ing']);
+//
+// //semester 4
+// $sem4_agama = mysql_real_escape_string($_POST['sem4_agama']);
+// $sem4_b_indo = mysql_real_escape_string($_POST['sem4_b_indo']);
+// $sem4_mtk = mysql_real_escape_string($_POST['sem4_mtk']);
+// $sem4_ipa = mysql_real_escape_string($_POST['sem4_ipa']);
+// $sem4_b_ing = mysql_real_escape_string($_POST['sem4_b_ing']);
+//
+// //semester 5
+// $sem5_agama = mysql_real_escape_string($_POST['sem5_agama']);
+// $sem5_b_indo = mysql_real_escape_string($_POST['sem5_b_indo']);
+// $sem5_mtk = mysql_real_escape_string($_POST['sem5_mtk']);
+// $sem5_ipa = mysql_real_escape_string($_POST['sem5_ipa']);
+// $sem5_b_ing = mysql_real_escape_string($_POST['sem5_b_ing']);
 
 
 $rapor_edit = mysqli_query($koneksi, "UPDATE tb_siswa SET
