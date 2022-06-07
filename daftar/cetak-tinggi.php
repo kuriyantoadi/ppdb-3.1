4391e3841b86eb9a17da63a84ea9c62268b03f41
kuriyantoadi <melatikuning212@gmail.com>
2022-06-07 10:02:48 +0700

    update 7 juni 2022, update 5

--- daftar/cetak-tinggi.php
+++ daftar/cetak-tinggi.php
@@ -135,7 +135,7 @@ while ($d_siswa = mysqli_fetch_array($data_siswa)) {
       </tr>
       <tr>
         <td>Jarak Rumah ke Sekolah</td>
-        <td><?php echo $d['jarak_kesekolah']; ?> KiloMeter</td>
+        <td><?php echo $d['jarak_kesekolah']; ?> Kilo Meter</td>
       </tr>
       <tr>
         <td>Nama Orang Tua / Wali</td>
