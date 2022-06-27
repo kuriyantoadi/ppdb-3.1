c21d491f2b789d2a812f715819a8c8b30aa85f73
kuriyantoadi <melatikuning212@gmail.com>
2022-06-28 01:44:30 +0700

    update 28 juni 2022, update 1

---
+++ daftar/form-daftar-ulang.php
@@ -0,0 +1,41 @@
+<tr>
+    <th>Informasi Alur Daftar Ulang</th>
+    <td>
+        <span style='margin-bottom: 5px;' class='label label-danger'>Mohon untuk Alur Daftar Ulang di Download dan Dibaca</span><br>
+        <a href="../assets/file/Alur_daftar_ulang.pdf" type="button" class="btn btn-primary btn-sm">
+            <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download
+        </a>
+    </td>
+</tr>
+<tr>
+    <th>Hasil Pengumuman</th>
+    <td>
+        <a href="" type="button" class="btn btn-primary btn-sm">
+            <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Cetak
+        </a>
+    </td>
+</tr>
+<tr>
+    <th>Formulir Dapodik</th>
+    <td>
+        <a href="../assets/file/F-PESERTA_DIDIK.pdf" type="button" class="btn btn-primary btn-sm">
+            <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Cetak
+        </a>
+    </td>
+</tr>
+<tr>
+    <th>Pakta Integritas</th>
+    <td>
+        <a href="" type="button" class="btn btn-primary btn-sm">
+            <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Cetak
+        </a>
+    </td>
+</tr>
+<tr>
+    <th>Tanda Terima Berkas Daftar Ulang</th>
+    <td>
+        <a href="" type="button" class="btn btn-primary btn-sm">
+            <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Cetak
+        </a>
+    </td>
+</tr>
