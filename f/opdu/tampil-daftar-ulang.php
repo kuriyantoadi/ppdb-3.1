e17d3a11858ee6811b31fc320b9632f72282d3d3
kuriyantoadi <melatikuning212@gmail.com>
2022-07-05 09:46:26 +0700

    update 5 juni 2022, update 1

--- f/opdu/tampil-daftar-ulang.php
+++ f/opdu/tampil-daftar-ulang.php
@@ -3,7 +3,7 @@
 
 $t_daftar_ulang= $d['kondisi_daftar_ulang'];
 if ($t_daftar_ulang == "Selesai") {
-  echo "<span style='margin-right: 5px;' class='label label-success'>Diterima</span>";
+  echo "<span style='margin-right: 5px;' class='label label-success'>Selesai</span>";
 }elseif ($t_daftar_ulang == "Belum") {
   echo "<span style='margin-right: 5px;' class='label label-default'>Belum</span>";
 }else {
